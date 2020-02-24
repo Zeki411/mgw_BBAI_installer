c27afa148db43bac69de934724d7cf0ea750001f
Zeki411 <42540848+Zeki411@users.noreply.github.com>
2020-02-25 05:34:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,7 +22,17 @@ sleep 1
 
 cd /opt/mgateway/lora_gateway/
 
-./reset_lgw.sh start
+# Reset PIN
+SX1301_RESET_BCM_PIN=75
+echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
+echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+sleep 0.1
+echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+sleep 0.1
+echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+sleep 0.1
+echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
 
 sleep 2
 
